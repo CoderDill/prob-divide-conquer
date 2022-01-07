c62011dbd90e34fbb40ac432a1afef2afcb1dc06
CrappyDill <mddillon47@gmail.com>
2022-01-06 22:06:27 -0800

    update

--- find-rotated-index.js
+++ find-rotated-index.js
@@ -1,6 +1,5 @@
-function findRotatedIndex(arr, num, low = 0, high = arr.length -1) {
-    if (low >= high) return -1;
-    if (high <  )
+function findRotatedIndex(arr, target) {
+    
 }
 
 module.exports = findRotatedIndex
