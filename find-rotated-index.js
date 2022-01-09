a822a54c8dcc2eb1b3db12fbb32a612e115d1198
CrappyDill <mddillon47@gmail.com>
2022-01-08 19:08:31 -0800

    findRotatedIdx start

--- find-rotated-index.js
+++ find-rotated-index.js
@@ -1,5 +1,11 @@
 function findRotatedIndex(arr, target) {
-    
+  let start = 0;
+  let end = arr.length - 1;
+  const mid = Math.floor((start + end) / 2);
+
+  while (start < end) {
+    if (arr[mid] === target) return mid;
+  }
 }
 
-module.exports = findRotatedIndex
+module.exports = findRotatedIndex;
