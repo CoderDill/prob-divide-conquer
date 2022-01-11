e2aa564fa6d4d32f3c2f1bad157566bf2fbbc598
Matthew Dillon <mddillon47@yahoo.com>
2022-01-10 23:04:31 -0800

    Update sorted-frequency.js

--- sorted-frequency.js
+++ sorted-frequency.js
@@ -1,4 +1,19 @@
-function sortedFrequency() {
+function sortedFrequency(arr, num) {
+    let firstIdx = findFirstIdx(arr, num)
+    if (firstIdx === -1) return firstIdx
+    let lastIdx = findLastIdx(arr, num)
+    return lastIdx - firstIdx + 1
+}
+
+function findFirstIdx(arr, num, start=0, end=arr.length-1) {
+    while(start >= end) {
+        let mid = Math.floor((start + en) / 2)
+
+        if()
+    }
+}
+
+function lastIdx(arr, num) {
 
 }
 
