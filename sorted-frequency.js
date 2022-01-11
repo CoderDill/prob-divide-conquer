2baeb5b58c5cbcbf6d1dca77cd998c89fb48c508
Matthew Dillon <mddillon47@yahoo.com>
2022-01-11 00:10:53 -0800

    Update sorted-frequency.js

--- sorted-frequency.js
+++ sorted-frequency.js
@@ -6,15 +6,34 @@ function sortedFrequency(arr, num) {
 }
 
 function findFirstIdx(arr, num, start=0, end=arr.length-1) {
-    while(start >= end) {
-        let mid = Math.floor((start + en) / 2)
+    while(start <= end) {
+        let mid = Math.floor((start + end) / 2)
 
-        if()
+        if(mid === 0 || num > arr[mid - 1] && arr[mid] === num) {
+            return mid
+        }else if (num > arr[mid]) {
+            return findFirstIdx(arr, num, mid + 1, end)
+        } else {
+            return findFirstIdx(arr, num, start, mid - 1)
+        }
     }
+    return -1
 }
 
-function lastIdx(arr, num) {
-
+function findLastIdx(arr, num, start=0, end=arr.length - 1) {
+    while(start <= end) {
+        let mid = Math.floor((start + end) / 2)
+        
+        if((mid === arr.length - 1 || arr[mid] < arr[mid + 1]) && arr[mid] === num) {
+            return mid;
+        }else if (num < arr[mid]) {
+            return findLastIdx(arr, num, start, mid - 1)
+        } else {
+            return findLastIdx(arr, num, mid + 1, end)
+        }
+        return -1
+   }
 }
+
 
 module.exports = sortedFrequency
